de1101ab001fe9c22946424b784d30b076d72214
Savchuk <100482478+abdabdab@users.noreply.github.com>
2024-03-13 15:23:54 +0200

    Add new imports

--- app/src/main/java/com/example/ipz_cw_3_savchuk_andriy/MainActivity.kt
+++ app/src/main/java/com/example/ipz_cw_3_savchuk_andriy/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.ipz_cw_3_savchuk_andriy
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.ipz_cw_3_savchuk_andriy.ui.theme.IPZ_CW_3_Savchuk_AndriyTheme
 
